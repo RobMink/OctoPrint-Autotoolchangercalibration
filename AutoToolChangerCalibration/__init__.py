b515dc63309e57702a1f533fefb51c157c778636
Rob Mink <rob.mink@gmail.com>
2020-10-16 20:03:07 -0400

    gah

--- AutoToolChangerCalibration/__init__.py
+++ AutoToolChangerCalibration/__init__.py
@@ -1,19 +1,20 @@
 # coding=utf-8
-from __future__ import absolute_import, unicode_literals
-
-### (Don't forget to remove me)
-# This is a basic skeleton for your plugin's __init__.py. You probably want to adjust the class name of your plugin
-# as well as the plugin mixins it's subclassing from. This is really just a basic skeleton to get you started,
-# defining your plugin as a template plugin, settings and asset plugin. Feel free to add or remove mixins
-# as necessary.
-#
-# Take a look at the documentation on what other plugin mixins are available.
+#from __future__ import absolute_import, unicode_literals
 
 import octoprint.plugin
-
-class AutotoolchangercalibrationPlugin(octoprint.plugin.SettingsPlugin,
-                                       octoprint.plugin.AssetPlugin,
-                                       octoprint.plugin.TemplatePlugin):
+import datetime
+import logging
+import octoprint.plugin
+import octoprint.plugin.core
+import glob
+import os
+import sys
+class AutotoolchangercalibrationPlugin( octoprint.plugin.StartupPlugin,
+ octoprint.plugin.TemplatePlugin,
+ octoprint.plugin.SettingsPlugin,
+ octoprint.plugin.AssetPlugin,
+ octoprint.plugin.SimpleApiPlugin,
+ octoprint.plugin.EventHandlerPlugin):
 
 
 	##~~ SettingsPlugin mixin
@@ -67,9 +68,7 @@ __plugin_name__ = "Autotoolchangercalibration Plugin"
 # Starting with OctoPrint 1.4.0 OctoPrint will also support to run under Python 3 in addition to the deprecated
 # Python 2. New plugins should make sure to run under both versions for now. Uncomment one of the following
 # compatibility flags according to what Python versions your plugin supports!
-#__plugin_pythoncompat__ = ">=2.7,<3" # only python 2
-#__plugin_pythoncompat__ = ">=3,<4" # only python 3
-__plugin_pythoncompat__ = ">=2.7,<4" # python 2 and 3
+__plugin_pythoncompat__ = ">=2.7,<4"
 
 def __plugin_load__():
 	global __plugin_implementation__
--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@
 plugin_identifier = "AutoToolChangerCalibration"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
-plugin_package = "octoprint_AutoToolChangerCalibration"
+plugin_package = "AutoToolChangerCalibration"
 
 # The plugin's human readable name. Can be overwritten within OctoPrint's internal data via __plugin_name__ in the
 # plugin module
-plugin_name = "OctoPrint-Autotoolchangercalibration"
+plugin_name = "Autotoolchangercalibration"
 
 # The plugin's version. Can be overwritten within OctoPrint's internal data via __plugin_version__ in the plugin module
-plugin_version = "0.1"
+plugin_version = "0.11"
 
 # The plugin's description. Can be overwritten within OctoPrint's internal data via __plugin_description__ in the plugin
 # module
-plugin_description = """TODO"""
+plugin_description = "A self calibration plugin for tool changers using Klipper"
 
 # The plugin's author. Can be overwritten within OctoPrint's internal data via __plugin_author__ in the plugin module
 plugin_author = "RobMink"
